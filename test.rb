512ed3014020a481a4397241cfc3f83dd6020b9d
America Calleros <america.quintero@gmail.com>
2017-04-28 09:58:23 -0500

    Removing blank spaces

--- test.rb
+++ test.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/env ruby
-
-
 def geotag(id)
   puts "999%06d" % id
 end
-
-
 require 'set'
-
 def rand_n(n, max)
     randoms = Set.new
     loop do
@@ -15,6 +10,5 @@ def rand_n(n, max)
         return randoms.to_a.join.to_s if randoms.size >= n
     end
 end
-
 random = rand_n(6, 9)
 puts random
